a4708ede48d6ad5f0d8e9c44abac197598001263
Norton-breman <marin_antz@yahoo.fr>
2025-10-01 12:12:44 +0200

    fix bug in __read_data when there is no point data

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pupyC3D',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.16.0'
