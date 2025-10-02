1c30dd535ef7a5c0f9e07e4e1dca2909ce645bfc
Norton-breman <marin_antz@yahoo.fr>
2025-10-02 09:08:09 +0200

    fix bug in reading multidimensional numerical parameter

--- pupyC3D/c3dfile.py
+++ pupyC3D/c3dfile.py
@@ -2,7 +2,7 @@
 #
 # pupyC3D is licensed under a
 # Creative Commons Attribution-NonCommercial 4.0 International License.
-#
+
 # You should have received a copy of the license along with this
 # work. If not, see <https://creativecommons.org/licenses/by-nc/4.0/>.
 
